30c6e302551e07a5f1c401ce5a4f2778c043d4ec
Student <student@EPBYMINW0501.minsk.epam.com>
2019-02-24 20:42:21 +0300

    Homework2: Pavel Sauchanka

--- PythonTask2/Task1.py
+++ PythonTask2/Task1.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     StudentMarks = dict()
     N = int(input("Enter count of students = "))
     for i in range(N):
-        InputInformation = str(input("Write line with student Name and Marks: ")).split(' ')
-        InputName, InputMarks = InputInformation[0], map(float, InputInformation[1:])
+        InputInf = str(input("Write line with student Name and Marks: ")).split(' ')
+        InputName, InputMarks = InputInf[0], map(float, InputInf[1:])
         StudentMarks[InputName] = InputMarks
     YourChoice = input("Needed name of student: ")
     if YourChoice in StudentMarks.keys():
--- PythonTask2/Task2.py
+++ PythonTask2/Task2.py
@@ -1,12 +1,12 @@
 if __name__ == '__main__':
     Answer = dict()
-    list_for_values = list(input("Enter values of list: ").split(' '))
+    list4values = list(input("Enter values of list: ").split(' '))
     list_for_keys = list(input("Enter keys of list: ").split(' '))
-    if len(list_for_values) < len(list_for_keys):
+    if len(list4values) < len(list_for_keys):
         #  Insert None, if list doesn't have values for such keys count
-        list_for_values.extend([None]*(len(list_for_keys)-len(list_for_values)))
+        list4values.extend([None] * (len(list_for_keys)-len(list4values)))
     for i in range(len(list_for_keys)):
-        keys, values = list_for_keys[i], list_for_values[i]
+        keys, values = list_for_keys[i], list4values[i]
         Answer[keys] = values
     print("Your constructed library: ")
     print(Answer)
