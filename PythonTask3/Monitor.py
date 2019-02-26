eb3856fc3f6e13162d96de4d0e494cfb24370bb1
Student <student@EPBYMINW0501.minsk.epam.com>
2019-02-26 05:22:50 +0300

    Homework3: Pavel Sauchanka1

--- PythonTask3/Monitor.py
+++ PythonTask3/Monitor.py
@@ -16,14 +16,14 @@ class MonitorUtil:
         total_memory = psutil.virtual_memory().total / 1024 ** 3
         used_memory = psutil.virtual_memory().used / 1024 ** 3
         cpu_load = psutil.cpu_percent(interval=Interval)
-        network_inf_bytes_sent = psutil.net_io_counters().bytes_sent / 1024 ** 3
-        network_inf_bytes_recv = psutil.net_io_counters().bytes_recv / 1024 ** 3
-        network_inf_packets_sent = psutil.net_io_counters().packets_sent
-        network_inf_packets_recv = psutil.net_io_counters().packets_recv
-        network_inf_errin = psutil.net_io_counters().errin
-        network_inf_errout = psutil.net_io_counters().errout
-        network_inf_dropin = psutil.net_io_counters().dropin
-        network_inf_dropout = psutil.net_io_counters().dropout
+        net_inf_bytes_sent = psutil.net_io_counters().bytes_sent / 1024 ** 3
+        net_inf_bytes_recv = psutil.net_io_counters().bytes_recv / 1024 ** 3
+        net_inf_packets_sent = psutil.net_io_counters().packets_sent
+        net_inf_packets_recv = psutil.net_io_counters().packets_recv
+        net_inf_errin = psutil.net_io_counters().errin
+        net_inf_errout = psutil.net_io_counters().errout
+        net_inf_dropin = psutil.net_io_counters().dropin
+        net_inf_dropout = psutil.net_io_counters().dropout
         io_inf_read_count = psutil.disk_io_counters().read_count
         io_inf_write_count = psutil.disk_io_counters().write_count
         io_inf_read_bytes = psutil.disk_io_counters().read_bytes / 1024 ** 3
@@ -33,20 +33,20 @@ class MonitorUtil:
         output = ["Total memory = %s GB\n" % total_memory,
                   "Used memory = %s GB\n" % used_memory,
                   "CPU Load (in percents) = %s\n" % cpu_load,
-                  "Number of Gbytes sent = %s\n" % network_inf_bytes_sent,
-                  "Number of Gbytes sent = %s\n" % network_inf_bytes_recv,
-                  "Number of packets sent = %s\n" % network_inf_packets_sent,
-                  "Number of packets received = %s\n" % network_inf_packets_recv,
-                  "Total number of errors while receiving = %s\n" % network_inf_errin,
-                  "Total number of errors while sending = %s\n" % network_inf_errout,
-                  "Total number of incoming packets which were dropped = %s\n" % network_inf_dropin,
-                  "Total number of outgoing packets which were dropped = %s\n" % network_inf_dropout,
+                  "Number of Gbytes sent = %s\n" % net_inf_bytes_sent,
+                  "Number of Gbytes sent = %s\n" % net_inf_bytes_recv,
+                  "Number of packets sent = %s\n" % net_inf_packets_sent,
+                  "Number of packets received = %s\n" % net_inf_packets_recv,
+                  "Total number of errors (receiving) = %s\n" % net_inf_errin,
+                  "Total number of errors (sending) = %s\n" % net_inf_errout,
+                  "Total number of incompacks(dropped) = %s\n" % net_inf_dropin,
+                  "Total number of outpacks(dropped) = %s\n" % net_inf_dropout,
                   "Number of reads = %s\n" % io_inf_read_count,
                   "Number of writes = %s\n" % io_inf_write_count,
                   "Number of Gbytes read = %s\n" % io_inf_read_bytes,
                   "Number of Gbytes written = %s\n" % io_inf_write_bytes,
-                  "Time spent reading from disk (in seconds) = %s\n" % io_inf_read_time,
-                  "Time spent writing to disk (in seconds) = %s\n" % io_inf_write_time]
+                  "Time spent reading from disk(sec) = %s\n" % io_inf_read_time,
+                  "Time spent writing to disk(sec)= %s\n" % io_inf_write_time]
         time_stamp = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
         snap_print = "SNAPSHOT %s : %s\n" % (str(MonitorUtil.temp), time_stamp)
         with open(OutputFile, "a") as f:
