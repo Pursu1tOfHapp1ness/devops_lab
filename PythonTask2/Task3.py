9efc85b8257d3590f5545ec5766f21445f448a6f
Pavel Savchenko <44941016+Pursu1tOfHapp1ness@users.noreply.github.com>
2019-02-27 19:25:38 +0300

    Update Task3.py

--- PythonTask2/Task3.py
+++ PythonTask2/Task3.py
@@ -6,9 +6,9 @@ def factorial(n):
 
 
 if __name__ == "__main__":
-    Input = open(r'C:\\Users\\unles\\Desktop\\INPUT.txt')
+    Input = open('INPUT.txt')
     num = int(Input.read())
-    Output = open(r'C:\\Users\\unles\\Desktop\\OUTPUT.txt', "w")
+    Output = open('OUTPUT.txt', "w")
     Output.write(str(factorial(num)))
     Input.close()
     Output.close()
